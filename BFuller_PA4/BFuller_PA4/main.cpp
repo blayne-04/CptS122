707d26714b3ee2109bd833ef0ff63788c47ddfdf
Blayne Fuller <befuller2004@gmail.com>
2026-02-23 10:58:53 -0800

    Added header block to main

--- BFuller_PA4/BFuller_PA4/main.cpp
+++ BFuller_PA4/BFuller_PA4/main.cpp
@@ -1,5 +1,16 @@
 #include "header.hpp"
 
+/****************************************
+* Author: Blayne Fuller
+*
+* Term: SPR26
+* Course: CptS122
+* Assignment: EXERCISE & DIET PLANNER - PA4
+*
+* Description: This program uses classes in
+* C++ to load, modify, and store exercise
+* and diet plans
+****************************************/
 int main(void) {
 
 }
